e5b5d66338726a5440788ebefc0c554672abced8
aamree <adilet90@mail.ru>
2019-08-27 16:23:54 +0600

    SignInForm commit

---
+++ src/components/routes/AuthPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignInForm from "../auth/SignInForm";
+
+function AuthPage() {
+  return (
+    <div>
+      <h1>AuthPage</h1>
+      <SignInForm />
+    </div>
+  );
+}
+
+export default AuthPage;
